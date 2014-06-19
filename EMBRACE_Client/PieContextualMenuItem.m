d290e852760312a83bcf4916caa7648f5954aad9
Administrator <Administrator@en4085125.mobile.asu.edu>
2014-06-19 11:08:03 -0700

    Draw X over the last image in the sorted array to fix issue with X being drawn over the sheep when it eats the apple

--- EMBRACE_Client/PieContextualMenuItem.m
+++ EMBRACE_Client/PieContextualMenuItem.m
@@ -228,7 +228,10 @@
                 //If this is an object that should be disappearing, go ahead and draw an X over it.
                 //That means this image corresponds to the first image in the unsorted array.
                 //TODO: Fix this based on the current changes.
-                if((itemImage == [images objectAtIndex:0]) && ((type == DISAPPEAR) || (type == TRANSFERANDDISAPPEAR))) {
+                //if((itemImage == [images objectAtIndex:0]) && ((type == DISAPPEAR) || (type == TRANSFERANDDISAPPEAR))) {
+                //If this is an object that should be disappearing, go ahead and draw an X over it.
+                //That means this image corresponds to the last image in the sorted array
+                if((itemImage == [sortedImages lastObject]) && ((type == DISAPPEAR) || (type == TRANSFERANDDISAPPEAR))) {
                     //Create the view that paints the X over the top of the image.
                     XView *xView = [[XView alloc] initWithFrame:CGRectMake(imageTopLeftX, imageTopLeftY, imageView.frame.size.width, imageView.frame.size.height)];
                     [self addSubview:xView];
