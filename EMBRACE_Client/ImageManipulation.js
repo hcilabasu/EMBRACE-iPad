9d14b294e90d99371121f1ceaffc704d09b3da0b
jprodri3 <James@en4092548.local>
2015-06-07 20:07:55 -0700

    3

--- EMBRACE_Client/ImageManipulation.js
+++ EMBRACE_Client/ImageManipulation.js
@@ -811,8 +811,6 @@ function addSentence(sentenceId, action, splitTextArray, wordsArray) {
     textbox.appendChild(newSentence); //add sentence to textbox
 }
 
-
-
 function getImagePosition (object) {
     var position = new Array();
     position[0] = object.offsetLeft;
