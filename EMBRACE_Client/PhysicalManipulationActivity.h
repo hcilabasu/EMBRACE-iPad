8e2a2187c7cef867ba203d6f91b6e3142d0c12a9
jprodri3 <James@en4092548.local>
2015-06-07 20:27:47 -0700

    6

--- EMBRACE_Client/PhysicalManipulationActivity.h
+++ EMBRACE_Client/PhysicalManipulationActivity.h
@@ -8,16 +8,24 @@
 
 #import "Activity.h"
 #import "ActionStep.h"
+#import "AlternateSentence.h"
 #import "PhysicalManipulationSolution.h"
 
 @interface PhysicalManipulationActivity : Activity {
     NSMutableDictionary *setupSteps;
-    PhysicalManipulationSolution *PMSolution;
+    NSMutableDictionary *alternateSentences;
+    NSMutableDictionary *PMSolutions;
+    //PhysicalManipulationSolution *PMSolution;
 }
 
 @property (nonatomic, strong) NSMutableDictionary *setupSteps;
-@property (nonatomic, strong) PhysicalManipulationSolution *PMSolution;
+@property (nonatomic, strong) NSMutableDictionary *alternateSentences;
+@property (nonatomic, strong) NSMutableDictionary *PMSolutions;
+//@property (nonatomic, strong) PhysicalManipulationSolution *PMSolution;
 
 - (void) addSetupStep:(ActionStep*)setupStep forPageId:(NSString*)pageId;
+- (void) addAlternateSentence:(AlternateSentence*)altSent forPageId:(NSString*)pageId;
+- (void) addPMSolution:(PhysicalManipulationSolution*)PMSolution forActivityId:(NSString*)activityId;
 
 @end
+
--- EMBRACE_Client/PhysicalManipulationActivity.m
+++ EMBRACE_Client/PhysicalManipulationActivity.m
@@ -11,12 +11,14 @@
 @implementation PhysicalManipulationActivity
 
 @synthesize setupSteps;
-@synthesize PMSolution;
+@synthesize alternateSentences;
+@synthesize PMSolutions;
 
 - (id) init {
     if (self = [super init]) {
         setupSteps = [[NSMutableDictionary alloc] init];
-        PMSolution = [[PhysicalManipulationSolution alloc] init];
+        alternateSentences = [[NSMutableDictionary alloc] init];
+        PMSolutions = [[NSMutableDictionary alloc] init];
     }
     
     return self;
@@ -37,6 +39,44 @@
     //If it does, we just add the setup step to the array.
     else {
         [setupStepsForKey addObject:setupStep];
+    }
+}
+
+//Add alternate sentence to specific page with id
+- (void) addAlternateSentence:(AlternateSentence*)altSent forPageId:(NSString*)pageId {
+    //Check to see if the key (page id) exists.
+    //If it doesn't, we add the key with a new NSMutableArray that will contain the alternate sentence created.
+    NSMutableArray* altSentsForKey = [alternateSentences objectForKey:pageId];
+    
+    if (altSentsForKey == nil) {
+        altSentsForKey = [[NSMutableArray alloc] init];
+        [altSentsForKey addObject:altSent];
+        
+        if ([pageId length] != 0)
+            [alternateSentences setObject:altSentsForKey forKey:pageId];
+    }
+    //If it does, we just add the alternate sentence to the array.
+    else {
+        [altSentsForKey addObject:altSent];
+    }
+}
+
+//Add PhysicalManipulationSolution to specific activity (page) with id
+- (void) addPMSolution:(PhysicalManipulationSolution*)PMSolution forActivityId:(NSString*)activityId {
+    //Check to see if the key (activity id) exists.
+    //If it doesn't, we add the key with a new NSMutableArray that will contain the PMSolution created.
+    NSMutableArray* PMSolutionsForKey = [PMSolutions objectForKey:activityId];
+    
+    if (PMSolutionsForKey == nil) {
+        PMSolutionsForKey = [[NSMutableArray alloc] init];
+        [PMSolutionsForKey addObject:PMSolution];
+        
+        if ([activityId length] != 0)
+            [PMSolutions setObject:PMSolutionsForKey forKey:activityId];
+    }
+    //If it does, we just add the PMSolution to the array.
+    else {
+        [PMSolutionsForKey addObject:PMSolution];
     }
 }
 
