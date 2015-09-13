cd598424e33a17e669bf1a9336088032c979ae07
jprodri3 <James@en4092548.local>
2015-09-13 11:52:02 -0700

    loading bottled joy

--- EMBRACE_Client/AppDelegate.m
+++ EMBRACE_Client/AppDelegate.m
@@ -96,6 +96,15 @@
     newPath = [newPath stringByAppendingString:filename];
     [fileManager copyItemAtPath:filePath toPath:newPath error:nil];
     
+    filePath = [[NSBundle mainBundle] pathForResource:@"bottled" ofType:@"epub"];
+    newPath = [documentsDirectory stringByAppendingPathComponent:@"/ASU/IntroToBottled"];
+    
+    [fileManager createDirectoryAtPath:newPath withIntermediateDirectories:YES attributes:nil error:nil];
+    
+    filename = [@"/" stringByAppendingString:[filePath lastPathComponent]];
+    newPath = [newPath stringByAppendingString:filename];
+    [fileManager copyItemAtPath:filePath toPath:newPath error:nil];
+    
     return YES;
 }
 
