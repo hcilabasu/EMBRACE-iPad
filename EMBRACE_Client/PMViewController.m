69d4c3a73c9420ce15242793b935d2526f71b58e
jithin <jroy6@asu.edu>
2016-04-28 11:40:04 -0700

    - Fix for audio not playing for chapter title.

--- EMBRACE_Client/PMViewController.m
+++ EMBRACE_Client/PMViewController.m
@@ -4508,8 +4508,10 @@ BOOL wasPathFollowed = false;
                 if (audio) {
                     NSString *spanishAudio = nil;
                     
-                   
-                    if ([ConditionSetup sharedInstance].language == BILINGUAL && conditionSetup.newInstructions) {
+                    // IntroIpadReads_IM does not have spanish audio.
+                    if (![audio isEqualToString:@"IntroIpadReads_IM"] &&
+                        [ConditionSetup sharedInstance].language == BILINGUAL &&
+                        conditionSetup.newInstructions) {
                         spanishAudio = [NSString stringWithFormat:@"%@_S.mp3",audio];
                         
                     }
