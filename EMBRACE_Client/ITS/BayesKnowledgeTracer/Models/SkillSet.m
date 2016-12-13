ef565b5a702eb4e6bdfdb28d4758871239371369
Audrey Wong <aewong@asu.edu>
2016-12-12 21:05:18 -0700

    Decreased initial easy syntax skill value

--- EMBRACE_Client/ITS/BayesKnowledgeTracer/Models/SkillSet.m
+++ EMBRACE_Client/ITS/BayesKnowledgeTracer/Models/SkillSet.m
@@ -30,7 +30,7 @@
         _medSyntaxSkill = (SyntaxSkill *) [Skill syntaxSkillWithComplexity:EM_Medium];
         _complexSyntaxSkill = (SyntaxSkill *) [Skill syntaxSkillWithComplexity:EM_Complex];
         
-        [_easySyntaxSkill updateSkillValue:0.99];
+        [_easySyntaxSkill updateSkillValue:0.9];
         
         _usabilitySkill = (UsabilitySkill *) [Skill usabilitySkill];        
     }
