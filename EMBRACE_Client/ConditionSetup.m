ae47793ef04281d0cc14701721971885c7f2a92a
Shang Wang <shangwang@shangs-macbook-pro.local>
2017-09-18 13:50:30 -0700

    Fixing: ITS003 and  ITS004, “Why does the ball fall to the ground,” but now this sentence is missing!

--- EMBRACE_Client/ConditionSetup.m
+++ EMBRACE_Client/ConditionSetup.m
@@ -68,7 +68,7 @@ static ConditionSetup *sharedInstance = nil;
         
         allowFileSync = YES;
         
-        ITSComplexity=ITS_SYSTEM;
+        ITSComplexity=ITS_EASY;
         //allow testers to fast skip sentences, change to YES if you want to skip sentences fast
         fastSkipSentence=YES;
     }
