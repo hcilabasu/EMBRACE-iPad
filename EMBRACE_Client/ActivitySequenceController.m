e86458fe95d92c9713c95be05fef1d14ee189117
Shang Wang <swang158@asu.edu>
2017-09-26 15:05:41 -0700

    Fixing mandarin study sequences.

--- EMBRACE_Client/ActivitySequenceController.m
+++ EMBRACE_Client/ActivitySequenceController.m
@@ -99,7 +99,7 @@
     
     // MANDARIN Study sequence
     else if ([participantCode rangeOfString:@"MAN"].location != NSNotFound && [[participantCode componentsSeparatedByString:@"MAN"][1] length] == 3) {
-        numSequences = 4;
+        numSequences = 2;
         
         //Get number at end of participant code and match it to appropriate sequence
         NSInteger sequenceNumber = [[participantCode componentsSeparatedByString:@"MAN"][1] integerValue] % numSequences;
